b2a68a212b11d0bb0dea2074dbaa8ce0a7887701
Kingsley Andrew <andrewkingsley1000@gmail.com>
2026-07-01 01:11:26 +0100

    Add files via upload

---
+++ Bank fraud.sql
@@ -0,0 +1,93 @@
+﻿Total_Transactions,Total_Fraud_Cases,Fraud_Rate_Percentage,Total_Transaction_Amount,Total_Fraud_Amount,Avg_Transaction_Amount
+1000000,55255,5.53,204724665.42,12602509.65,204.72
+
+(1 row affected)
+
+Transaction_Type,Total_Transactions
+Legitimate,944745
+Fraud,55255
+
+(2 rows affected)
+
+country,Total_Fraud_Cases
+USA,16617
+India,6600
+UK,5499
+Brazil,4552
+Germany,4424
+Canada,3879
+Japan,3845
+France,3785
+Mexico,3295
+Australia,2759
+
+(10 rows affected)
+
+merchant_category,Total_Fraud_Cases
+Jewelry,5821
+ATM Withdrawal,5802
+Crypto Exchange,5737
+Fuel,3234
+Electronics,3230
+Education,3195
+Clothing,3183
+Entertainment,3151
+Healthcare,3143
+Online Shopping,3139
+Travel,3139
+Restaurant,3137
+Gaming,3134
+Grocery,3121
+Utilities,3089
+
+(15 rows affected)
+
+payment_method,Total_Fraud_Cases
+Credit Card,19568
+Debit Card,16420
+Bank Transfer,8261
+Mobile Payment,6568
+Crypto,2758
+Cheque,1680
+
+(6 rows affected)
+
+hour_of_day,Total_Fraud_Cases
+0,3271
+1,3360
+2,3297
+3,3315
+4,3303
+5,3359
+6,3345
+7,1648
+8,1764
+9,1807
+10,1680
+11,1732
+12,1670
+13,1620
+14,1653
+15,1669
+16,1673
+17,1672
+18,1766
+19,1667
+20,1687
+21,1708
+22,3326
+23,3263
+
+(24 rows affected)
+
+device_type,Total_Fraud_Cases
+Mobile,25024
+Desktop,13665
+ATM,5661
+Tablet,5470
+POS Terminal,5435
+
+(5 rows affected)
+
+
+Completion time: 2026-06-30T23:47:55.5483394+01:00
